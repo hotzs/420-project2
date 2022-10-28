879243a47f45c693936ff7b995ee3bfd532af817
Stephanie <hotzs@lafayette.edu>
2022-10-28 16:07:50 -0400

    blah

--- constraint.py
+++ constraint.py
@@ -1,4 +1,6 @@
 class constraint:
     def __init__(self, constraint_str):
         self.constraint_str = constraint_str
-        
+
+    def constraint_ret():
+        #find a way to store a constraint not as a string
