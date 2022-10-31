8d6e10a6c4cf54c139d8a9e763f0219aa629b833
Stephanie <hotzs@lafayette.edu>
2022-10-31 19:01:20 -0400

    decided on constraint

--- constraint.py
+++ constraint.py
@@ -1,6 +1,11 @@
-class constraint:
-    def __init__(self, constraint_str):
-        self.constraint_str = constraint_str
+# class constraint:
+#     def __init__(self):
+#         self.contraints[100]
 
-    def constraint_ret():
-        #find a way to store a constraint not as a string
+
+#     def constraint_ret():
+#         #find a way to store a constraint not as a string
+#         print("Cannot be ")
+#     def add_constraint(self,num):
+#         self.constraint.append(num)
+    
